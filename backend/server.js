2ee5de47fe162f96d03d463df1aff6b9bb78a940
khawaidev <khawaidev@gmail.com>
2025-07-12 10:47:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Basic route
 const classifyRoute = require('./routes/classify');
 
